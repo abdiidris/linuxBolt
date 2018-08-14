bed31931b228351c4fcc60976c0d2a818fd45bc3
Abdi <abdiidris14@gmail.com>
2018-08-14 01:49:23 +0100

    project structure improved

---
+++ linuxDash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linuxDash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
